be01090aa2f3c077a57a11a75a5528af616b2271
Mel <melanie.llorente@estudiantes.utec.edu.uy>
2023-10-24 16:40:35 -0300

    Clase e Interfaz de Institucion

---
+++ Aplicacion/ServidorApp/src/main/java/codigocreativo/uy/servidorapp/servicios/InstitucionRemote.java
@@ -0,0 +1,15 @@
+package codigocreativo.uy.servidorapp.servicios;
+
+import codigocreativo.uy.servidorapp.entidades.Institucion;
+import codigocreativo.uy.servidorapp.entidades.Usuario;
+import jakarta.ejb.Remote;
+
+import java.util.List;
+
+@Remote
+public interface InstitucionRemote {
+    public void agregarInstitucion(Institucion i);
+    public void eliminarInstitucion(Institucion i);
+    public void modificarInstitucion(Institucion i);
+    public List<Ubicacion> obtenerUbicaciones();
+}
