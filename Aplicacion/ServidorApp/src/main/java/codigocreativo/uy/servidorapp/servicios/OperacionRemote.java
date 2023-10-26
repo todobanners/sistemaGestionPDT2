2b0a6d3af03ce5abb1977df9a7c7b7348f31bd57
Mel <melanie.llorente@estudiantes.utec.edu.uy>
2023-10-26 00:24:32 -0300

    Hice los beans de auditoria y operacion

--- Aplicacion/ServidorApp/src/main/java/codigocreativo/uy/servidorapp/servicios/OperacionRemote.java
+++ Aplicacion/ServidorApp/src/main/java/codigocreativo/uy/servidorapp/servicios/OperacionRemote.java
@@ -5,7 +5,7 @@ import codigocreativo.uy.servidorapp.entidades.Operacion;
 public interface OperacionRemote {
     public void crearOperacion(Operacion o);
     public void modificarOperacion (Operacion o);
-
+    
     public void eliminarOperacion (Operacion o);
-
+    
 }
