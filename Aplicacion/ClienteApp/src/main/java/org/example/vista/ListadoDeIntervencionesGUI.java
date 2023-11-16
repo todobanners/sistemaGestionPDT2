f8928721f869d0d615673c775e78aa73d4909578
Mel <melanie.llorente@estudiantes.utec.edu.uy>
2023-11-15 22:13:21 -0300

    Modifico Swing de Listado de Intervenciones, y le agrego los date picker

--- Aplicacion/ClienteApp/src/main/java/org/example/vista/ListadoDeIntervencionesGUI.java
+++ Aplicacion/ClienteApp/src/main/java/org/example/vista/ListadoDeIntervencionesGUI.java
@@ -1,26 +1,50 @@
 package org.example.vista;
 
+import codigocreativo.uy.servidorapp.entidades.TiposIntervencione;
 import codigocreativo.uy.servidorapp.servicios.IntervencionRemote;
+import com.toedter.calendar.JDateChooser;
+import org.example.modelo.Conexion;
+import org.jdesktop.swingx.JXTable;
 
+import javax.naming.NamingException;
 import javax.swing.*;
 
 public class ListadoDeIntervencionesGUI {
     private JPanel panelListadoDeIntervenciones;
     private JPanel panelAcciones;
-    private JTable tableListadoIntervenciones;
-    private JTextField textFechaDesde;
-    private JTextField textFechaHasta;
     private JTextField textIdEquipo;
-    private JButton generarReporteButton;
+    private JButton generarReportePorFechaButton;
     private JComboBox comboTipoDeIntervencion;
+    private JButton filtroBoton;
+    private JButton limpiarButton;
+    private JScrollPane panelTabla;
+    private JXTable tablaPerfiles;
+    private JButton generarReportePorTipoButton;
+
+    JDateChooser textFechaDesde = new JDateChooser();
+    JDateChooser textFechaHasta = new JDateChooser();
 
     private IntervencionRemote intervencionRemoteBean;
+
+
 
     public JPanel getPanel(){
         return panelListadoDeIntervenciones;
     }
 
-    public ListadoDeIntervencionesGUI() {
+    public ListadoDeIntervencionesGUI() throws NamingException {
+
         JFrame frame = new JFrame(" Listado de Intervenciones");
+
+        try {
+            intervencionRemoteBean = Conexion.obtenerIntervencionBean();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "No se pudo conectar con el servidor");
+        }
+
+        //obtener los tipos de intervencines
+        for (TiposIntervencione tipoIntervencione : Conexion.obtenerTiposIntervencionBean().obtenerTiposIntervenciones()) {
+            comboTipoDeIntervencion.addItem(tipoIntervencione);
+        }
     }
 }
