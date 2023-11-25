f6a0d5dc18269b3f1991384b8e9b6bad369c5c47
Eduardo Gimeno <eduardo.gimeno@estudiantes.utec.edu.uy>
2023-11-25 08:05:49 -0300

    Interfaz Movimiento equipos
    Signed-off-by: Eduardo Gimeno <eduardo.gimeno@estudiantes.utec.edu.uy>

--- Aplicacion/ClienteApp/src/main/java/org/example/vista/Ubicacion/MovimientoEquiposGUI.java
+++ Aplicacion/ClienteApp/src/main/java/org/example/vista/Ubicacion/MovimientoEquiposGUI.java
@@ -15,4 +15,16 @@ public class MovimientoEquiposGUI {
     private JComboBox comboBox5;
     private JPanel contenedorFechaFiltroDesde;
     private JPanel contenedoFechaFiltroHasta;
+    private JComboBox comboBox6;
+    private JTextField textField1;
+    private JComboBox comboBox7;
+    private JComboBox comboBox8;
+    private JComboBox comboBox9;
+    private JTextField textField2;
+    private JComboBox comboBox10;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton guardarButton;
+    private JButton cancelarButton;
 }
